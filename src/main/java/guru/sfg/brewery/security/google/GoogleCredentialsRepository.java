63a8f5d8df187860d17241769a073a36b14f9231
Marcus Blackhall <marcusinamsterdam@gmail.com>
2022-06-13 17:02:55 +0200

    Add google authenticator
    Use warren strange library

---
+++ src/main/java/guru/sfg/brewery/security/google/GoogleCredentialsRepository.java
@@ -0,0 +1,39 @@
+package guru.sfg.brewery.security.google;
+
+import com.warrenstrange.googleauth.ICredentialRepository;
+import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class GoogleCredentialsRepository implements ICredentialRepository {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public String getSecretKey(String username) {
+
+        User user = userRepository.findByUsername(username).orElseThrow();
+        return user.getGoogle2FaSecret();
+    }
+
+    @Override
+    public void saveUserCredentials(String userName, String secretKey, int validationCode, List<Integer> scratchCodes) {
+
+        User user = userRepository.findByUsername(userName).orElseThrow();
+
+        user.setGoogle2FaSecret(secretKey);
+        user.setUserGoogle2fa(true);
+
+        userRepository.save(user);
+
+
+
+    }
+}
